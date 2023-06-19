62971af2a06bc81e8df2545386cb6d0436b17644
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-06-19 19:58:55 -0400

    first commit

---
+++ server/models/trails.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+// example model with validation
+const TrailModel = new mongoose.Schema(
+    {
+        trailName: {
+            type: String,
+            required: [true, "Trail name is required"],
+            minLength: [2, "The name must be 2 or more characters"],
+            maxLength: [50, "The name can't be over 50 characters"],
+        },
+        distance: {
+            type: Number,
+            required: [true, "Release year is required"],
+            min: [1, "No trail shorter than 1 mile allowed"],
+        },
+        description: {
+            type: String,
+            required: [true, "Description is required"],
+            minLength: [3, "The description must be 3 or more characters"],
+            maxLength: [500, "The description can't be over 500 characters"],
+        },
+        difficalty: {
+            type: Number,
+        },
+        wheelchairAccess: {
+            type: Boolean,
+            required: [true, "Wheelchair accessibility is required"],
+        },
+        image: {
+            type: String,
+        },
+    },
+    // { timestamps: true } will create timestamp, of course
+    { timestamps: true }
+);
+
+const Trail = mongoose.model("Trail", TrailModel);
+
+module.exports = Trail;
